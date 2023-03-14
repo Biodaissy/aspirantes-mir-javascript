f533d7bab1f32f057f053684a824061668e5f3c6
Biodaissy <daissymvelandia@gmail.com>
2023-03-14 17:46:35 -0500

    solucion a los ejercicios de js

---
+++ Hello.js
@@ -0,0 +1,15 @@
+//Ejercicio 1
+// function hola(nombre) {
+//   const frase = "Hola " + nombre + "!";
+//   console.log(nombre);
+//   return frase;
+// }
+function hola(nombre) {
+  return "Hola " + nombre + "!";
+}
+console.log(hola("Pedro")); // "Hola Pedro!"
+console.log(hola("Juan")); // "Hola Juan!"
+console.log(hola("")); // "Hola !"
+
+// const fraseExterna = hola("Juan");
+// console.log("fraseExterna");
---
+++ js-1/Sum.js
@@ -0,0 +1,12 @@
+// escribe la función suma acá
+function suma(number) {
+  let resultado = 0;
+  resultado = (number * (number + 1)) / 2;
+  return resultado;
+}
+
+// código de prueba
+
+console.log(suma(4)); // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)); // 55
+console.log(suma(15)); // 120
